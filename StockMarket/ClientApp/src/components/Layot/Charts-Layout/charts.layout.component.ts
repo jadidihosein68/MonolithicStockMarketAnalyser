bf4841360c40e7af71e9fc959f757aa93251a99e
Hossein <jadidi.hosein@gmail.com>
2018-12-16 13:41:06 +0800

    add chart layout component

---
+++ StockMarket/ClientApp/src/components/Layot/Charts-Layout/charts.layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Chart-layout',
+  templateUrl: './charts.layout.component.html',
+  styleUrls: ['./charts.layout.component.scss']
+})
+export class ChartsLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
