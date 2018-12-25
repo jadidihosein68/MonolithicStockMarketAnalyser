b9157ce0a0371a53c9c0e003aff98d27146fefab
Hossein <jadidi.hosein@gmail.com>
2018-12-25 18:44:16 +0800

    now data can get from twitter and store temporarly in the table

--- StockMarket/ClientApp/src/components/search-tweets/search.tweets.component.ts
+++ StockMarket/ClientApp/src/components/search-tweets/search.tweets.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { TweetsSummary } from '../../model/interface/TweetsSummary';
+import { Component, ViewChild } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { TwitterService } from '../../services/TwitterService';
 import { sweetAlertService } from '../../services/sweetAlertService.service';
+import { TwitterDatatableComponent } from '../datatables/TwitterDatatable/twitter.datatable.component';
 
 @Component({
   selector: 'Search-Twitter',
@@ -11,6 +13,9 @@ import { sweetAlertService } from '../../services/sweetAlertService.service';
 export class SearchTweetsComponent {
   closeResult: string;
   TweetID:string ;
+  @ViewChild(TwitterDatatableComponent)
+  twitterDatatableComponent:TwitterDatatableComponent;
+
   constructor(private modalService: NgbModal
     , private TwitterService: TwitterService
     , private sweetAlertService: sweetAlertService
@@ -19,13 +24,22 @@ export class SearchTweetsComponent {
 
   async getTweet() {
     let Tweets = await this.TwitterService.getTweets(this.TweetID);
+
     this.sweetAlertService.getSwal({
       type: 'success',
       title: 'Tweets !',
       text: `A Total No. of ${Tweets.totalNoOfTweets} Tweets were extracted`,
       footer: '<a >Dont tell anyone !</a>'
     });
+
     this.modalService.dismissAll();
+
+    let tweettssammury :TweetsSummary = {
+      Screen_Name : Tweets.screenName,
+      NOofRecords : Tweets.totalNoOfTweets,
+      LastUpdate : new Date()
+    } 
+    this.twitterDatatableComponent.addRow(tweettssammury);
   }
 
 
