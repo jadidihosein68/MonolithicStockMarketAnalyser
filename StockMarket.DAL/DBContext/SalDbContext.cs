7dc4b811052333e63b4b3d2dade03a656dd5f155
Hossein <jadidi.hosein@gmail.com>
2018-12-26 01:47:54 +0800

    add DBContext

---
+++ StockMarket.DAL/DBContext/SalDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace StockMarket.DAL.DBContext
+{
+    public class SalDbContext : DbContext
+    {
+        public SalDbContext(DbContextOptions<SalDbContext> options)
+            : base(options)
+        { }
+
+        // public DbSet<Blog> Blogs { get; set; }
+    }
+}
--- StockMarket.Model/Configuration/AppConfiguration.cs
+++ StockMarket.Model/Configuration/AppConfiguration.cs
@@ -6,8 +6,9 @@ namespace StockMarket.Model.Configuration
 {
     public class AppConfiguration
     {
-        public string QuandlAPIKey { get; set;}
-        public Endpoints Endpoints { get;set;}
+        public string QuandlAPIKey { get; set; }
+        public Endpoints Endpoints { get; set; }
         public TwitterCrodential TwitterCrodential { get; set; }
+        public string ConnectionString { get; set;}
     }
 }
