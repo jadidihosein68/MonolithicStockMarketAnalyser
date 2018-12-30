768318ccdf26a9195985c6aa4558f81d42b82d9f
Hossein <jadidi.hosein@gmail.com>
2018-12-30 23:12:27 +0800

    Add DB context Add Fluent API

---
+++ StockMarket.DAL/DBContext/ISalDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockMarket.DAL.DBContext
+{
+    public interface ISalDbContext
+    {
+    }
+}
--- StockMarket.DAL/DBContext/SalDbContext.cs
+++ StockMarket.DAL/DBContext/SalDbContext.cs
@@ -1,14 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StockMarket.DAL.Persistence.EntityConfigurations;
+using StockMarket.Model;
 using System.Collections.Generic;
 
 namespace StockMarket.DAL.DBContext
 {
-    public class SalDbContext : DbContext
+    public class SalDbContext : DbContext , ISalDbContext
     {
         public SalDbContext(DbContextOptions<SalDbContext> options)
             : base(options)
         { }
+        
+        // Models
+        public DbSet<Tweet> Tweet { get; set; }
 
-        // public DbSet<Blog> Blogs { get; set; }
+        // Views 
+        public DbQuery<TweetsSummary> TweetsSummary { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Query<TweetsSummary>().ToView("View_TweetsSummary");
+
+            modelBuilder.ApplyConfiguration(new TweetConfigration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
     }
 }
