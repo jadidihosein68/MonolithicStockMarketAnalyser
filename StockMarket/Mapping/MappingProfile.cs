762664f24a4fac3e035175d8b0982b093c0691d0
Hossein <jadidi.hosein@gmail.com>
2019-01-05 13:21:37 +0800

    add mapping profile

---
+++ StockMarket/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq;
+using StockMarket.Model.DTO;
+using StockMarket.Model.Base;
+
+namespace StockMarket.Core.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RowHistoricalStockBase, RowHistoricalStockBaseDTO>();
+        }
+    }
+}
