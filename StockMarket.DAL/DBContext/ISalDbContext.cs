5a98af9034c7944de06f51b0d90fa7d7b835b788
Hossein <jadidi.hosein@gmail.com>
2019-01-05 13:21:08 +0800

    update DB to use new model

--- StockMarket.DAL/DBContext/ISalDbContext.cs
+++ StockMarket.DAL/DBContext/ISalDbContext.cs
@@ -4,14 +4,24 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using StockMarket.Model;
 using StockMarket.Model.Base;
+using StockMarket.Model.Quantitative;
 
 namespace StockMarket.DAL.DBContext
 {
     public interface ISalDbContext
     {
+        DbQuery<TweetsSummary> TweetsSummary { get; set; }
+        DbQuery<StockSummary> StockSummary { get; set; }
+
         DbSet<Tweet> Tweet { get; set; }
         DbSet<RowHistoricalStockBase> TimeSeries { get; set; }
-        DbQuery<TweetsSummary> TweetsSummary { get; set; }
+
+        DbSet<StockFocuse> StockFocuse { get; set; }
+        DbSet<TimeSeries> TimeSeriesReal { get; set; }
+        DbSet<MACDIndex> MACDIndex { get; set; }
+        DbSet<RSIIndex> RSIIndex { get; set; }
+        DbSet<GuppyIndex> GuppyIndex { get; set; }
+        DbSet<SOIndex> SOIndex { get; set; }
 
     }
 }
--- StockMarket.DAL/DBContext/SalDbContext.cs
+++ StockMarket.DAL/DBContext/SalDbContext.cs
@@ -2,6 +2,7 @@
 using StockMarket.DAL.Persistence.EntityConfigurations;
 using StockMarket.Model;
 using StockMarket.Model.Base;
+using StockMarket.Model.Quantitative;
 using System.Collections.Generic;
 
 namespace StockMarket.DAL.DBContext
@@ -15,22 +16,35 @@ namespace StockMarket.DAL.DBContext
         // Models
         public DbSet<Tweet> Tweet { get; set; }
         public DbSet<StockFocuse> StockFocuse { get; set; }
-        public DbSet<StockLabel> StockLabel { get; set; }
         public DbSet<RowHistoricalStockBase> TimeSeries { get; set; }
+        public DbSet<TimeSeries> TimeSeriesReal { get; set; }
+        public DbSet<MACDIndex> MACDIndex { get; set; }
+        public DbSet<RSIIndex> RSIIndex { get; set; }
+        public DbSet<GuppyIndex> GuppyIndex { get; set; }
+        public DbSet<SOIndex> SOIndex { get; set; }
+
+
+
+
 
 
         // Views 
         public DbQuery<TweetsSummary> TweetsSummary { get; set; }
+        public DbQuery<StockSummary> StockSummary { get; set; }
+
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder
-                .Query<TweetsSummary>().ToView("View_TweetsSummary");
+            modelBuilder.Query<TweetsSummary>().ToView("View_TweetsSummary");
+            modelBuilder.Query<StockSummary>().ToView("View_StockSummary");
 
             modelBuilder.ApplyConfiguration(new TweetConfigration());
             modelBuilder.ApplyConfiguration(new StockFocuseConfigration());
-            modelBuilder.ApplyConfiguration(new StockLabelConfigration());
+           // modelBuilder.ApplyConfiguration(new StockLabelConfigration());
             modelBuilder.ApplyConfiguration(new TimeSeriesConfigration());
+            modelBuilder.ApplyConfiguration(new TimeSeriesRealConfigration());
+            //  modelBuilder.ApplyConfiguration(new TimeSeriesMACDConfigration());
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
