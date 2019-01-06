2dd8fce50b450d0dd5beb94f758d38135e4b90bf
Hossein <jadidi.hosein@gmail.com>
2019-01-06 22:46:17 +0800

    update repository

--- StockMarket.Repository/Concreate/TimeSeriesRepository.cs
+++ StockMarket.Repository/Concreate/TimeSeriesRepository.cs
@@ -1,69 +1,77 @@
-﻿using StockMarket.Adapter.Interface;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StockMarket.Adapter.Interface;
 using StockMarket.DAL.Interface.Persistance;
 using StockMarket.DAL.Interface.Persistance.Repositories;
 using StockMarket.Model;
 using StockMarket.Model.Base;
+using StockMarket.Model.Quantitative;
 using StockMarket.Repository.Interface;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace StockMarket.Repository.Concreate
-{
-    public class TimeSeriesRepository : ITimeSeriesRepository
-    {
+namespace StockMarket.Repository.Concreate {
+    public class TimeSeriesRepository : ITimeSeriesRepository {
         private readonly IQuandlHistoricalStockAdapter IHistoricalStockAdapter;
         private readonly IUnitOfWork IUnitOfWork;
 
-        public TimeSeriesRepository(IQuandlHistoricalStockAdapter _IHistoricalStockAdapter,
+        public TimeSeriesRepository (IQuandlHistoricalStockAdapter _IHistoricalStockAdapter,
             IUnitOfWork _IUnitOfWork
-            )
-        {
+        ) {
             IHistoricalStockAdapter = _IHistoricalStockAdapter;
             IUnitOfWork = _IUnitOfWork;
         }
 
+        public IEnumerable<RowHistoricalStockBase> GetQuandlData (RequestHistoricalStockQuandl RequestHistoricalStockQuandl) {
 
-        public IEnumerable<RowHistoricalStockBase> GetQuandlData(RequestHistoricalStockQuandl RequestHistoricalStockQuandl)
-        {
-
-            var result = IHistoricalStockAdapter.getCSVFromQuandl(RequestHistoricalStockQuandl);
+            var result = IHistoricalStockAdapter.getCSVFromQuandl (RequestHistoricalStockQuandl);
             return result;
         }
 
-        public IEnumerable<TimeSeriesIndex> GetQuandlDataIndex(RequestHistoricalStockQuandl RequestHistoricalStockQuandl)
-        {
+        public IEnumerable<TimeSeriesIndex> GetQuandlDataIndex (RequestHistoricalStockQuandl RequestHistoricalStockQuandl) {
 
-            var result = IHistoricalStockAdapter.getCSVFromQuandlIndex(RequestHistoricalStockQuandl);
+            var result = IHistoricalStockAdapter.getCSVFromQuandlIndex (RequestHistoricalStockQuandl);
             return result;
         }
 
-        public string getStringFromQuandl(RequestHistoricalStockQuandl RequestHistoricalStockQuandl)
-        {
+        public string getStringFromQuandl (RequestHistoricalStockQuandl RequestHistoricalStockQuandl) {
 
-            var result = IHistoricalStockAdapter.getStringFromQuandl(RequestHistoricalStockQuandl);
+            var result = IHistoricalStockAdapter.getStringFromQuandl (RequestHistoricalStockQuandl);
             return result;
         }
 
-
-        public IEnumerable<RowHistoricalStockBase> getTimeSeriesFromDB(string StockIndex)
-        {
-            return IUnitOfWork.TimeSeriesDAL.getTimeSeriesByStockIndex(StockIndex);
+        public IEnumerable<RowHistoricalStockBase> getTimeSeriesFromDB (string StockIndex) {
+            return IUnitOfWork.TimeSeriesDAL.getTimeSeriesByStockIndex (StockIndex);
         }
 
-        public void AddRangeToDB(IEnumerable<RowHistoricalStockBase> RowHistoricalStockBase)
-        {
-            IUnitOfWork.TimeSeriesDAL.AddRange(RowHistoricalStockBase);
-            IUnitOfWork.Complite();
+        public void AddRangeToDB (IEnumerable<RowHistoricalStockBase> RowHistoricalStockBase) {
+            IUnitOfWork.TimeSeriesDAL.AddRange (RowHistoricalStockBase);
+            IUnitOfWork.Complite ();
         }
 
-
-        public void AddRangeIndexToDB(IEnumerable<TimeSeriesIndex> RowHistoricalStockBase)
-        {
-            IUnitOfWork.TimeSeriesDAL.AddRangeIndex(RowHistoricalStockBase);
-            IUnitOfWork.Complite();
+        public void AddRangeIndexToDB (IEnumerable<TimeSeriesIndex> RowHistoricalStockBase) {
+            IUnitOfWork.TimeSeriesDAL.AddRangeIndex (RowHistoricalStockBase);
+            IUnitOfWork.Complite ();
         }
 
+        public void AddRangeMACD (IEnumerable<MACDIndex> MACDIndex) {
+            IUnitOfWork.MACDDAL.AddRange (MACDIndex);
+            IUnitOfWork.Complite ();
+        }
 
-}
+        public void AddRangeRSI (IEnumerable<RSIIndex> RSIIndex) {
+            IUnitOfWork.RSIDAL.AddRange (RSIIndex);
+            IUnitOfWork.Complite ();
+        }
+
+        public void AddRangeSO (IEnumerable<SOIndex> SOIndex) {
+            IUnitOfWork.SODAL.AddRange (SOIndex);
+            IUnitOfWork.Complite ();
+        }
+
+        public void AddRangeGuppy (IEnumerable<GuppyIndex> GuppyIndex) {
+            IUnitOfWork.GUPPYDAL.AddRange (GuppyIndex);
+            IUnitOfWork.Complite ();
+        }
+
+    }
 }
--- StockMarket.Repository/Interface/ITimeSeriesRepository.cs
+++ StockMarket.Repository/Interface/ITimeSeriesRepository.cs
@@ -1,19 +1,23 @@
-﻿using StockMarket.Model;
-using StockMarket.Model.Base;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using StockMarket.Model;
+using StockMarket.Model.Base;
+using StockMarket.Model.Quantitative;
 
-namespace StockMarket.Repository.Interface
-{
-    public interface ITimeSeriesRepository
-    {
-        IEnumerable<TimeSeriesIndex> GetQuandlDataIndex(RequestHistoricalStockQuandl RequestHistoricalStockQuandl);
-        IEnumerable<RowHistoricalStockBase> GetQuandlData(RequestHistoricalStockQuandl RequestHistoricalStockQuandl);
-        string getStringFromQuandl(RequestHistoricalStockQuandl RequestHistoricalStockQuandl);
-        IEnumerable<RowHistoricalStockBase> getTimeSeriesFromDB(string StockIndex);
-        void AddRangeToDB(IEnumerable<RowHistoricalStockBase> RowHistoricalStockBase);
-        void AddRangeIndexToDB(IEnumerable<TimeSeriesIndex> RowHistoricalStockBase);
+namespace StockMarket.Repository.Interface {
+    public interface ITimeSeriesRepository {
+        IEnumerable<TimeSeriesIndex> GetQuandlDataIndex (RequestHistoricalStockQuandl RequestHistoricalStockQuandl);
+        IEnumerable<RowHistoricalStockBase> GetQuandlData (RequestHistoricalStockQuandl RequestHistoricalStockQuandl);
+        string getStringFromQuandl (RequestHistoricalStockQuandl RequestHistoricalStockQuandl);
+        IEnumerable<RowHistoricalStockBase> getTimeSeriesFromDB (string StockIndex);
+        void AddRangeToDB (IEnumerable<RowHistoricalStockBase> RowHistoricalStockBase);
+        void AddRangeIndexToDB (IEnumerable<TimeSeriesIndex> RowHistoricalStockBase);
+
+        void AddRangeGuppy (IEnumerable<GuppyIndex> GuppyIndex);
+        void AddRangeSO (IEnumerable<SOIndex> SOIndex);
+        void AddRangeRSI (IEnumerable<RSIIndex> RSIIndex);
+        void AddRangeMACD (IEnumerable<MACDIndex> MACDIndex);
 
     }
 }
