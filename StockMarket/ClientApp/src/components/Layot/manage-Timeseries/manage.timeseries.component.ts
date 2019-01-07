ceb29a008a116dc558898d8c2fafbef1dc165377
Hossein <jadidi.hosein@gmail.com>
2019-01-07 23:45:18 +0800

    add manage time series component

---
+++ StockMarket/ClientApp/src/components/Layot/manage-Timeseries/manage.timeseries.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'manage-timeseries',
+    templateUrl: './manage.timeseries.component.html',
+    styleUrls: ['./manage.timeseries.component.scss']
+})
+export class ManageTimeseriesComponent {
+    constructor() { }
+}
